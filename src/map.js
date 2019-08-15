3a79f204aa3cfe562600a2cd69d942c13dd2b729
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2019-08-15 21:17:44 +0300

    Make field responsive

--- src/map.js
+++ src/map.js
@@ -1,6 +1,14 @@
+import once from 'once';
+import { debounce } from 'debounce';
+
+const registerResizeEventOnce = once(registerResizeEvent);
+
 export function buildMap({ field, start, end }) {
   const mapEl = document.querySelector('.maze');
   cleanEl(mapEl);
+
+  setCellSize(field.length);
+  registerResizeEventOnce(field.length);
 
   mapEl.append(...generateMap(field));
 
@@ -55,4 +63,39 @@ function generateMap(field, start, finish) {
   });
 
   return rowsEls;
+}
+
+function calculateCellSize(length) {
+  const GLOBAL_MARGINS = 10;
+  const HEADER_HEIGHT = 30;
+  const RESULT_HEIGHT = 20;
+  const VERTICAL_GAP = 10;
+  const HORIZONTAL_FIX = 20;
+
+  const availableHeight =
+    window.innerHeight -
+    GLOBAL_MARGINS * 2 -
+    HEADER_HEIGHT -
+    RESULT_HEIGHT -
+    2 * VERTICAL_GAP;
+
+  const availableWidth =
+    window.innerWidth - 2 * GLOBAL_MARGINS - HORIZONTAL_FIX;
+
+  const shorterDimension = Math.min(availableHeight, availableWidth);
+
+  return Math.floor(shorterDimension / length);
+}
+
+function setCellSize(length) {
+  const cellSize = calculateCellSize(length);
+  document.documentElement.style.setProperty('--cell-size', `${cellSize}px`);
+}
+
+function registerResizeEvent(length) {
+  const handler = debounce(() => {
+    setCellSize(length);
+  }, 300);
+
+  window.addEventListener('resize', handler);
 }
